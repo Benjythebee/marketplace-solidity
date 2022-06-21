767f14d0d60f412e91eb0bd59f90d5ba4f967e1a
Benjythebee <Benjy.larcher@aol.com>
2022-06-22 08:14:49 +1200

    fix errors

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155ReceiverUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/introspection/ERC165CheckerUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
@@ -441,7 +440,6 @@ contract Marketplace is
         external
         view
         virtual
-        override
         returns (bool)
     {
         return interfaceId == type(IERC165Upgradeable).interfaceId;
